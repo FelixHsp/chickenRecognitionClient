a28dc0d0e9ef1849a6e2568f2669bf6df59bd29c
Felix <13115412392@163.com>
2019-09-07 19:26:34 +0800

    django图片上传

--- chickenClient/chickenapp/views.py
+++ chickenClient/chickenapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from chickenClient import settings
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def info(request):
+    if request.method == "POST":
+        f1 = request.FILES['pic1']
+        fname = '%s/pic/%s' % (settings.MEDIA_ROOT, f1.name)
+        with open(fname, 'wb') as pic:
+            for c in f1.chunks():
+                pic.write(c)
+        return HttpResponse("ok")
+    else:
+        return HttpResponse("error")
